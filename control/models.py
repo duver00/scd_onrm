cb41a6f94d4f9dde3a258e7754603161c984231e
Duver <duver089@gmail.com>
2022-02-23 22:43:55 -0500

    init gitignore + models

--- control/models.py
+++ control/models.py
@@ -1,22 +1,50 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class Direcciones(models.Model):
-    nombre = models.CharField(max_length=50)
-    correo = models.EmailField()
+    nombre = models.CharField(max_length=50, null=False, blank=False)
+    correo = models.EmailField(null=False, blank=False)
 
 
 class Organismo(models.Model):
-    nombre = models.CharField(max_length=25)
+    nombre = models.CharField(max_length=25, null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Organismo'
+        verbose_name_plural = 'Organismo'
 
 
 class Entidad(models.Model):
-    nombre = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100, null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Entidad'
+        verbose_name_plural = 'Entidades'
 
 
 class TipoDocumento(models.Model):
-    tipo = models.CharField(max_length=255)
+    tipo = models.CharField(max_length=255,null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Tipo de Documento'
+        verbose_name_plural = 'Tipos de Documentos'
+
+
+class Documento(models.Model):
+    no_entrada_doc = models.IntegerField(null=False,blank=False)
+    no_salida_doc = models.IntegerField(null=True, blank=True)
+    f_entrada_doc = models.DateField(null=False, blank=False)
+    f_salida_doc = models.DateField(null=True, blank=True)
+    titulo = models.CharField(max_length=255)
+    organismo = models.ForeignKey('Organismo', on_delete=models.CASCADE)
+    entidad = models.ForeignKey('Entidad', on_delete=models.CASCADE)
+    t_documento = models.ForeignKey('TipoDocumento', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Documento'
+        verbose_name_plural = 'Documentos'
 
 
